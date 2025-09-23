3b9a6f266884ce5a524a0d2494a0aa071bf71594
johanvr81 <johanvr@justsolve.solutions>
2025-09-23 20:29:07 +0200

    Commit

---
+++ The Four Sisters and the Duckbear/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Let's calculate how many eggs the each of the four sisters get!");
+
+Console.WriteLine("How many eggs was laid today?");
+var eggs = Console.ReadLine();
+int sistereggs = Convert.ToInt32(eggs) / 4;
+int duckbear = Convert.ToInt32(eggs) % 4;
+
+Console.WriteLine("Each sister gets: " + sistereggs + " and the duckbear: "+ duckbear);
---
+++ The Triangle Farmer/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Let's calculate the are of a Triangle!");
+
+Console.WriteLine("Enter the Base:");
+var basesize = Console.ReadLine();
+Console.WriteLine("Enter the Height;");
+var height = Console.ReadLine();
+
+double area = (Convert.ToDouble(basesize) * Convert.ToDouble(height)) / 2;
+
+Console.WriteLine("The Area is: "+ area +"m2");
+
+
