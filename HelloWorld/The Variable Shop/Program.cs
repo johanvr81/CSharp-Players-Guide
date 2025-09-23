fd4a80c7d50d39a898ab0b296ae70ab9f357a366
johanvr81 <johanvr@justsolve.solutions>
2025-09-23 19:57:00 +0200

    Commit

---
+++ HelloWorld/The Variable Shop/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+byte aSingleByte = 34;
+aSingleByte = 21;
+Console.WriteLine(aSingleByte);
+short aNumber = 5039;
+aNumber = 5;
+Console.WriteLine(aNumber);
+long aVeryBigNumber = 395904282569;
+aVeryBigNumber = 1596327895U;
+Console.WriteLine(aVeryBigNumber);
+
+int bigNumber = 1_000_000_000;
+bigNumber = 2;
+Console.WriteLine(bigNumber);
+
+char letter = 'a';
+Console.WriteLine(letter);
+string message = "Message String";
+Console.WriteLine(message);
+double number1 = 3.5623;
+Console.WriteLine(number1);
+float number2 = 3.5623f;
+Console.WriteLine(number2);
+decimal number3 = 3.5623m;
+Console.WriteLine(number3);
+bool booltest = true;
+Console.WriteLine(booltest);
+
