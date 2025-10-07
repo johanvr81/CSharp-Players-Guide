263e0c4ab3b5b68d8d239f3cda8cee66d3687ab0
johanvr81 <johanvr@justsolve.solutions>
2025-10-07 20:06:02 +0200

    Commit

---
+++ Vin Fletcher’s Arrows/Arrow.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vin_Fletcher_s_Arrows
+{    class Arrow
+    {
+        public ArrowHead _arrowHead;
+        public Fletching _fletching;
+        public float _length;
+
+        public Arrow(ArrowHead arrowHead, Fletching fletching, float length)
+        {
+            _arrowHead = arrowHead;
+            _fletching = fletching;
+            _length = length;
+        }
+
+        public float GetCost()
+        {
+            float ArrowHeadCost = _arrowHead switch
+            {
+                ArrowHead.Steel => 10,
+                ArrowHead.Wood => 3,
+                ArrowHead.Obsidian => 5
+            };
+
+            float fletchingCost = _fletching switch
+            {
+                Fletching.Plastic => 10,
+                Fletching.TurkeyFeathers => 5,
+                Fletching.GooseFeathers => 3
+            };
+
+            float shaftCost = 0.05f * _length;
+
+            return ArrowHeadCost + fletchingCost + shaftCost;
+        }
+    }
+
+    public enum ArrowHead
+    {
+        Steel,
+        Wood,
+        Obsidian
+    }
+
+    public enum Fletching
+    {
+        Plastic,
+        TurkeyFeathers,
+        GooseFeathers
+    }
+}
---
+++ Vin Fletcher’s Arrows/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Vin_Fletcher_s_Arrows;
+
+Console.Title = "Vin Fletcher’s Arrows";
+
+Arrow arrow = GetArrow();
+Console.WriteLine($"That arrow costs {arrow.GetCost()} gold.");
+
+Arrow GetArrow()
+{
+    ArrowHead arrowhead = GetArrowheadType();
+    Fletching fletching = GetFletchingType();
+    float length = GetLength();
+
+    return new Arrow(arrowhead, fletching, length);
+}
+
+ArrowHead GetArrowheadType()
+{
+    Console.Write("Arrowhead type (steel, wood, obsidian): ");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "steel" => ArrowHead.Steel,
+        "wood" => ArrowHead.Wood,
+        "obsidian" => ArrowHead.Obsidian
+    };
+}
+
+Fletching GetFletchingType()
+{
+    Console.Write("Fletching type (plastic, turkey feather, goose feather): ");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "plastic" => Fletching.Plastic,
+        "turkey feather" => Fletching.TurkeyFeathers,
+        "goose feather" => Fletching.GooseFeathers
+    };
+}
+
+float GetLength()
+{
+    float length = 0;
+
+    while (length < 60 || length > 100)
+    {
+        Console.Write("Arrow length (between 60 and 100): ");
+        length = Convert.ToSingle(Console.ReadLine());
+    }
+
+    return length;
+}
