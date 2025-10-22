f9b404cd7d7d8960263db2f26cf9561dd03c93a7
johanvr81 <johanvr@justsolve.solutions>
2025-10-22 15:19:44 +0200

    Commit

---
+++ The Fountain of Objects/Monster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Fountain_of_Objects
+{
+    public abstract class Monster
+    {
+        public Location Location { get; set; }
+
+        public bool IsAlive { get; set; } = true;
+
+        public Monster(Location start) => Location = start;
+
+        public abstract void Activate(FountainOfObjectsGame game);
+    }
+
+    public class Amarok : Monster
+    {
+        public Amarok(Location start) : base(start) { }
+
+        public override void Activate(FountainOfObjectsGame game) => game.Player.Kill("You have encountered an amarok and have died.");
+    }
+
+
+}
