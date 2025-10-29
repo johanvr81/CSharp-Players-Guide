6b40fcf4b766a54a3a09b5ef866289fbe5690f63
johanvr81 <johanvr@justsolve.solutions>
2025-10-29 12:36:25 +0200

    Commit

---
+++ Converting Directions to Offsets/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Title = "Converting Directions to Offsets";
+
+Direction direction = Direction.South;
+Console.WriteLine(direction);
+Console.WriteLine((BlockOffset)direction);
+
+public enum Direction { North, East, South, West }
+
+public record BlockOffset(int RowOffset, int ColumnOffset)
+{
+    public static implicit operator BlockOffset(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.North => new BlockOffset(-1, 0),
+            Direction.South => new BlockOffset(+1, 0),
+            Direction.West => new BlockOffset(0, -1),
+            Direction.East => new BlockOffset(0, +1)
+        };
+    }
+}
+
+public record BlockCoordinate(int Row, int Column);
+
+//I made the conversion implicit since there’s no data loss. Interestingly, I still had to use an explicit cast in main to actually trigger it.
---
+++ Indexing Operand City/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Title = "Indexing Operand City";
+
+BlockCoordinate coordinate = new BlockCoordinate(4, 3);
+
+Console.WriteLine(coordinate[0]);
+Console.WriteLine(coordinate[1]);
+
+public enum Direction { North, East, South, West }
+public record BlockOffset(int RowOffset, int ColumnOffset);
+public record BlockCoordinate(int Row, int Column)
+{
+    public int this[int index] => index switch { 0 => Row, 1 => Column };
+}
+
+
+//I don’t think so. coordinate.Row is clearer than coordinate[0].
+//Indexers are best when the valid options aren’t known at compile time—like arrays, lists, or dictionaries, where elements or keys are determined at runtime.I don’t think so. coordinate.Row is clearer than coordinate[0].
+//Indexers are best when the valid options aren’t known at compile time—like arrays, lists, or dictionaries, where elements or keys are determined at runtime.
